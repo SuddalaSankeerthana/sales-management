a95452e4ebe1daef84b04414afce136e75b09e17
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-08-21 01:25:25 +0530

    modified invoice.ts

--- src/invoice.ts
+++ src/invoice.ts
@@ -1,5 +1,9 @@
+import { getStoreDiscountCaluculation } from "./calcualtions/storeCalculations";
 import { CustomerDataJson } from "./interfaces";
-import { getDeliveryHTML } from "./invoiceHtml";
+import { getContactDetails } from "./invoiceSections/invoiceContact";
+import { getInvoiceHeader } from "./invoiceSections/invoiceHeader";
+import { getSummaryHTML } from "./invoiceSections/invoiceSummery";
+import { getItemsTable } from "./invoiceSections/itemsTable/invoiceTable";
 
 export const PERCENTAGE_FACTOR = 0.01;
 export const GST_PERCENTAGE = { essential: 1, luxery: 5, default: 10 };
@@ -12,6 +16,14 @@ export var totalAmount = 0,
 export function addToTotalAmountWith(amount: number) {
   totalAmount += amount;
 }
+
+export function initialize(){
+  totalAmount = 0,
+  totalItemsDiscountAmount = 0,
+  totalGst = 0,
+  totalItemsPrice = 0;
+}
+
 export function addToTotalItemsDiscountAmountWith(discountAmount: number) {
   totalItemsDiscountAmount += discountAmount;
 }
@@ -28,13 +40,26 @@ export function subToTotalItemPriceWith(amount: number) {
   totalItemsPrice -= amount;
 }
 
-const getInvoice = function getInvoice(customerData: CustomerDataJson): string {
-  (totalAmount = 0),
-    (totalItemsDiscountAmount = 0),
-    (totalGst = 0),
-    (totalItemsPrice = 0);
-  let html = getDeliveryHTML(customerData);
+export function getInvoiceHTML(customerData: CustomerDataJson): string {
+  initialize()
+  let html = `${getInvoiceHeader()}
+  <body>
+    ${getContactDetails(
+      customerData.storeName,
+      customerData.address,
+      customerData.name,
+      customerData.phoneNumber
+  )}
+    ${getItemsTable(customerData)}`;
+  let { storeDiscount, storeDiscountAmount } = getStoreDiscountCaluculation(
+      customerData.storeDiscounts
+  );
+  html += `
+   ${getSummaryHTML(
+      storeDiscount,
+      storeDiscountAmount,
+      customerData.paymentMethod
+  )}
+    `;
   return html;
-};
-
-module.exports = getInvoice;
+}
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,17 @@
 import fs from "fs";
 import { CustomerDataJson } from "./interfaces";
+import { getInvoiceHTML } from "./invoice";
 
-const getInvoiceFunction = require("./invoice");
-
-const jsonString = fs.readFileSync("./data/customerData.json", "utf-8");
-
+const jsonString = fs.readFileSync("./data/customerData-1.json", "utf-8");
+const jsonString2= fs.readFileSync("./data/customerData-2.json", "utf-8");
 const jsonFileData: CustomerDataJson = JSON.parse(jsonString);
+const jsonFileData2: CustomerDataJson = JSON.parse(jsonString2);
 
-let html = getInvoiceFunction(jsonFileData);
+let html = getInvoiceHTML(jsonFileData);
+let html1 = getInvoiceHTML(jsonFileData2);
+
 fs.writeFileSync("invoice.html", html);
+console.log("File generated");
+
+fs.writeFileSync("invoice1.html", html1);
 console.log("File generated");
