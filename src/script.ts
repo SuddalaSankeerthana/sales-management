d8970e0d6d634848ff6802e5cd44f12d4eecb79b
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-08-21 01:32:20 +0530

    Changed the path in writing the invoices

--- src/script.ts
+++ src/script.ts
@@ -3,15 +3,15 @@ import { CustomerDataJson } from "./interfaces";
 import { getInvoiceHTML } from "./invoice";
 
 const jsonString = fs.readFileSync("./data/customerData-1.json", "utf-8");
-const jsonString2= fs.readFileSync("./data/customerData-2.json", "utf-8");
+const jsonString2 = fs.readFileSync("./data/customerData-2.json", "utf-8");
 const jsonFileData: CustomerDataJson = JSON.parse(jsonString);
 const jsonFileData2: CustomerDataJson = JSON.parse(jsonString2);
 
 let html = getInvoiceHTML(jsonFileData);
 let html1 = getInvoiceHTML(jsonFileData2);
 
-fs.writeFileSync("invoice.html", html);
+fs.writeFileSync("build/invoice.html", html);
 console.log("File generated");
 
-fs.writeFileSync("invoice1.html", html1);
+fs.writeFileSync("build/invoice1.html", html1);
 console.log("File generated");
