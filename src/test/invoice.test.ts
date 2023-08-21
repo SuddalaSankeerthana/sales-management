62382ae0782ccd9bfce0f04ddfc318f9d3c0ae97
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-08-21 12:17:42 +0530

    Added invoice.test.ts and calculations.test.ts

--- src/test/invoice.test.ts
+++ src/test/invoice.test.ts
@@ -1,11 +1,18 @@
-import { realtedPercentageAmount } from "../calcualtions/percentageAmount";
+
+import fs from "fs";
 import { getInvoiceHeader } from "../invoiceSections/invoiceHeader";
 import { getStyles } from "../invoiceSections/invoiceStyling";
+import { getContactDetails } from "../invoiceSections/invoiceContact";
 
+const jsonString = fs.readFileSync("./data/customerData-1.json", "utf-8");
+const customerData=JSON.parse(jsonString);
 test("test for Header part of invoice", () => {
   expect(typeof getInvoiceHeader()).toBe("string");
   expect(typeof getStyles()).toBe("string");
 });
-test("test for percentage amount caluculations",()=>{
-    expect(realtedPercentageAmount(100,10)).toEqual(10);
-})
+test("test for invoice section-1",()=>{
+    expect(typeof getContactDetails( customerData.storeName,
+        customerData.address,
+        customerData.name,
+        customerData.phoneNumber)).toBe("string");
+});
